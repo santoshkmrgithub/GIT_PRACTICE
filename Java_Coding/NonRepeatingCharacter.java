0f00a7a95806fc4beea2f9660193c57ce96283b8
Santosh <santoshkmr1560@gmail.com>
2026-08-30 18:44:21 +0530

    nonrepeat

--- Java_Coding/NonRepeatingCharacter.java
+++ Java_Coding/NonRepeatingCharacter.java
@@ -1,27 +1,54 @@
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+// import java.util.*;
+// import java.util.function.Function;
+// import java.util.stream.Collectors;
 
-public class NonRepeatingCharacter {
+// public class NonRepeatingCharacter {
+
+//     public static void main(String[] args) {
+
+//         String str = "swiss";
+
+//         Character result = str.chars()
+//                 .mapToObj(c -> (char) c)
+//                 .collect(Collectors.groupingBy(
+//                         Function.identity(),
+//                         LinkedHashMap::new,
+//                         Collectors.counting()
+//                 ))
+//                 .entrySet()
+//                 .stream()
+//                 .filter(entry -> entry.getValue() == 1)
+//                 .map(Map.Entry::getKey)
+//                 .findFirst()
+//                 .orElse(null);
+
+//         System.out.println("First non-repeating character: " + result);
+
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatingCharacter {
 
     public static void main(String[] args) {
 
         String str = "swiss";
 
-        Character result = str.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(
-                        Function.identity(),
-                        LinkedHashMap::new,
-                        Collectors.counting()
-                ))
-                .entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() == 1)
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(null);
+        Map<Character, Integer> map = new LinkedHashMap<>();
 
-        System.out.println("First non-repeating character: " + result);
+        for (char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+            if (entry.getValue() == 1) {
+                System.out.println(
+                        "First non-repeating character: "
+                                + entry.getKey()
+                );
+                break;
+            }
+        }
     }
 }
