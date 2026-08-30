7ddce2cbf400f2078f254e42aaa1ec212bed5d25
Santosh <santoshkmr1560@gmail.com>
2026-08-30 12:52:28 +0530

    FirstHighestSalary

---
+++ Java_Coding/SecondHighest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SecondHighest {
+
+    public static void main(String[] args) {
+
+        // HashMap<EmployeeId, Salary>
+        Map<Integer, Integer> employees = new HashMap<>();
+
+        employees.put(101, 50000);
+        employees.put(102, 80000);
+        employees.put(103, 60000);
+        employees.put(104, 100000);
+        employees.put(105, 90000);
+        employees.put(106, 100000);
+        employees.put(107, 70000);
+
+        // Print HashMap
+        System.out.println("Employees: " + employees);
+    }
+}
